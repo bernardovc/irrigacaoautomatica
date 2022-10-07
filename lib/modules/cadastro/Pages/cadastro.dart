51b1e02e72487f37d1fe1701b3d35fb1c729f04a
Thauany <thauanysoares671@gmail.com>
2022-10-07 10:01:05 -0300

    teste

---
+++ lib/modules/cadastro/Pages/cadastro.dart
@@ -0,0 +1,19 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class cadastroUsu extends StatefulWidget {
+  const cadastroUsu({Key? key}) : super(key: key);
+
+  @override
+  State<cadastroUsu> createState() => _cadastroUsuState();
+}
+
+class _cadastroUsuState extends State<cadastroUsu> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
