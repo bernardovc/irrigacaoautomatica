63cc593f41a2bdb6c025a6d83a1b2ac53c01b9ef
Thauany <thauanysoares671@gmail.com>
2022-10-07 10:03:42 -0300

    teste

---
+++ lib/modules/cadastro/Pages/aaaaaaa.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class caralhogit extends StatefulWidget {
+  const caralhogit({Key? key}) : super(key: key);
+
+  @override
+  State<caralhogit> createState() => _caralhogitState();
+}
+
+class _caralhogitState extends State<caralhogit> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
